7f1367f1a1a752591c71bf89d589b61166c7b98f
Juan Jose Cordoba Marin <141663994+JuanCUDEM@users.noreply.github.com>
2024-03-22 14:26:40 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,23 @@
 //create web server
 var http = require('http');
+//create file system
 var fs = require('fs');
+//create path
 var path = require('path');
+//create mime
 var mime = require('mime');
+//create cache
 var cache = {};
 
 //send 404 error
-function send404(response){
+function send404(response) {
 	response.writeHead(404, {'Content-Type': 'text/plain'});
-	response.write('Error 404: resource not found.');
+	response.write('Error 404: resource not found');
 	response.end();
 }
 
 //send file data
-function sendFile(response, filePath, fileContents){
+function sendFile(response, filePath, fileContents) {
 	response.writeHead(
 		200,
 		{"content-type": mime.lookup(path.basename(filePath))}
@@ -22,27 +26,27 @@ function sendFile(response, filePath, fileContents){
 }
 
 //serve static files
-function serveStatic(response, cache, absPath){
-	//check if file is cached in memory
-	if(cache[absPath]){
+function serveStatic(response, cache, absPath) {
+	//check if file is in cache
+	if (cache[absPath]) {
 		//serve file from memory
 		sendFile(response, absPath, cache[absPath]);
-	}else{
+	} else {
 		//check if file exists
-		fs.exists(absPath, function(exists){
-			if(exists){
+		fs.exists(absPath, function(exists) {
+			if (exists) {
 				//read file from disk
-				fs.readFile(absPath, function(err, data){
-					if(err){
+				fs.readFile(absPath, function(err, data) {
+					if (err) {
 						send404(response);
-					}else{
-						//cache file in memory
+					} else {
+						//cache file
 						cache[absPath] = data;
 						//serve file read from disk
 						sendFile(response, absPath, data);
 					}
 				});
-			}else{
+			} else {
 				//send 404 response
 				send404(response);
 			}
@@ -51,27 +55,24 @@ function serveStatic(response, cache, absPath){
 }
 
 //create http server
-var server = http.createServer(function(request, response){
+var server = http.createServer(function(request, response) {
 	var filePath = false;
-
 	//determine HTML file to be served by default
-	if(request.url == '/'){
+	if (request.url == '/') {
 		filePath = 'public/index.html';
-	}else{
-		//translate URL path to relative file path
+	} else {
 		filePath = 'public' + request.url;
 	}
-
 	var absPath = './' + filePath;
-	//serve static file
+	//serve file
 	serveStatic(response, cache, absPath);
 });
 
-//start web server
-server.listen(3000, function(){
+//start server
+server.listen(3000, function() {
 	console.log("Server listening on port 3000.");
 });
 
-//set up socket.io server
+//create chat server
 var chatServer = require('./lib/chat_server');
 chatServer.listen(server);
